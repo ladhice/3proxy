2309999cddfed2cbbaa3d9c18b2232ee41e82bca
ladhice <62552988+ladhice@users.noreply.github.com>
2020-04-25 02:51:54 +0300

    qwe
    qwe

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -1,7 +1,7 @@
 version=0.8.13
 apt-get update && apt-get -y upgrade
 apt-get install gcc make git -y
-wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/z3APA3A/3proxy/archive/${version}.tar.gz
+wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/ladhice/3proxy/blob/master/${version}.tar.gz
 tar xzf 3proxy-${version}.tar.gz
 cd 3proxy-${version}
 make -f Makefile.Linux
@@ -18,3 +18,4 @@ cd /etc/init.d/
 wget --no-check-certificate  https://raw.github.com/ladhice/3proxy/master/3proxy
 chmod  +x /etc/init.d/3proxy
 update-rc.d 3proxy defaults
+/etc/init.d/3proxy start
