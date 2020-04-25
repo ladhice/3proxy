3e72f40a7dc83b76bd9dd27f1039c4128e70264f
ladhice <62552988+ladhice@users.noreply.github.com>
2020-04-25 16:32:39 +0300

    qwe
    qwe

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -9,10 +9,10 @@ cd src
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://raw.github.com/ladhice/3proxy/master/3proxy.cfg
+wget --no-check-certificate https://github.com/ladhice/3proxy/master/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
 mkdir /var/log/3proxy/
-wget --no-check-certificate https://raw.github.com/ladhice/3proxy/master/.proxyauth
+wget --no-check-certificate https://github.com/ladhice/3proxy/master/.proxyauth
 chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
 wget --no-check-certificate  https://raw.github.com/ladhice/3proxy/master/3proxy
